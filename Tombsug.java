6aaf47f9f7a30b67f249564fedb57ab53b50eb40
Szalai Alexandra <szandiszalai09@gmail.com>
2022-02-22 08:43:25 +0100

    Kezdes

---
+++ Tombsug.java
@@ -0,0 +1,31 @@
+/*
+* File: Tombsug.java
+* Author: Szalai Alexandra
+* Copyright: 2022, Szalai Alexandra
+* Group: Szoft I N
+* Date: 2022-02-22
+* Github: https://github.com/szalaialexandra/
+* Licenc: GNU GPL
+*/
+import java.util.Scanner;
+
+class Tombsug {
+	
+	public static void main (String[] args) {
+		System.out.println("Szalai Alexandra, 2022-02-22");
+		System.out.println("Feladat 0312");
+		System.out.println("Szoft I N");
+		System.out.println("");
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Oldal: ");
+		int oldal = sc.nextInt();
+		
+		System.out.print("Alfa szog: ");
+		int alfa_szog = sc.nextInt();
+		
+		
+		
+	}
+}
+
